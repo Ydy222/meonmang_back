214ac2973eef279aaa175be6245afe7eac4b8d64
DESKTOP-29VI7GO\user <kimyoott@gmail.com>
2025-06-04 11:28:50 +0900

    no message

--- main/app.js
+++ main/app.js
@@ -29,6 +29,7 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
 }));
+app.options("*", cors());
 app.use("/images", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
